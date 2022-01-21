fd641ac1ac47a484694da75cddf713b1012dd195
woqiufan <kiss.loo@qq.com>
2022-01-21 11:13:23 +0800

    1、添加pod

---
+++ Pods/Target Support Files/Pods-ProjectKit-ProjectKitUITests/Pods-ProjectKit-ProjectKitUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ProjectKit_ProjectKitUITests : NSObject
+@end
+@implementation PodsDummy_Pods_ProjectKit_ProjectKitUITests
+@end
---
+++ Pods/Target Support Files/Pods-ProjectKit/Pods-ProjectKit-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ProjectKit : NSObject
+@end
+@implementation PodsDummy_Pods_ProjectKit
+@end
